f145e935afb9a8ddbe1c61fc51d3168a6d1331b2
unknown <koos@cesbit.com>
2023-03-06 13:21:08 +0100

    lint

--- lib/check/indexchange.py
+++ lib/check/indexchange.py
@@ -12,7 +12,7 @@ async def check_indexchange(
         asset_config: dict,
         config: dict) -> dict:
 
-    # compatilibity level 90 is required for the using derived arguments when 
+    # compatilibity level 90 is required for the using derived arguments when
     # calling the dm_db_stats_properties dynamic view
     res = await get_data(asset, asset_config, config, QUERY, IDX, each_db=True,
                          min_compatibility_level=90)
